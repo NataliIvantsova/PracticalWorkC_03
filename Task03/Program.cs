9f2d1e27b4a341846275692f138725bbe5b4677e
NataliIvantsova <ivancovanm@yandex.ru>
2023-01-25 22:06:14 +0300

    Добавила решение задачи №3

---
+++ Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 3
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+int Enter(string message) // 
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());//возвращаемое значение из консоль редлайн в виде числа
+}
+
+int number = Enter("Введите число N ");
+//Math.Pow(n,3);// фукция выведения степени в куб, если 3
+int i = 1; // индекс равен единице
+
+while(i <= number)
+{
+    double result = Math.Pow(i,3);
+    System.Console.Write($"{result}  ");
+    i++;
+}
